756ed60316fb5bd1ec59cc048223a2167f927518
Khurshid Khan <khurshidsunny45@gmail.com>
2024-03-09 16:45:17 +0500

    made controlled elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,52 @@
 import Button from "./Button";
-import InputEl from "./InputEl";
 
-export default function Form() {
+export default function Form({
+  title,
+  description,
+  onSetTitle,
+  onSetDescription,
+  onAddTask,
+}) {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!title || !description) return;
+    const newTask = {
+      id: crypto.randomUUID(),
+      title,
+      description,
+    };
+
+    onAddTask(newTask);
+  }
   return (
-    <form className="flex items-center justify-center gap-4 px-4 py-2 pb-8 border-b-2 border-slate-700">
-      <InputEl title="Title" placeholder="What's the title" />
+    <form
+      className="flex items-center justify-center gap-4 px-4 py-2 pb-8 border-b-2 border-slate-700"
+      onSubmit={handleSubmit}
+    >
+      {/* input for title */}
+      <div className="flex flex-col">
+        <label>Title</label>
+        <input
+          value={title}
+          onChange={(e) => onSetTitle(e.target.value)}
+          type="text"
+          placeholder="what's the title"
+          className="px-3 py-2 rounded-md mt-2 w-full text-black"
+        />
+      </div>
 
-      <InputEl title="description" placeholder="what's the description" />
+      {/* input for description */}
+      <div className="flex flex-col">
+        <label>Description</label>
+        <input
+          value={description}
+          onChange={(e) => onSetDescription(e.target.value)}
+          type="text"
+          placeholder="what's the description"
+          className="px-3 py-2 rounded-md mt-2 w-full text-black"
+        />
+      </div>
 
       <Button bgColor="green-500">Add</Button>
     </form>
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import Form from "./Form";
 import NavigateButtons from "./NavigateButtons";
 import TodoList from "./TodoList";
 
+const initialTasks = [
+  { id: 1, title: "go to gym", description: "workout at 8 PM" },
+  { id: 2, title: "complete Assignment", description: "do all react projects" },
+];
+
 export default function TodoApp() {
+  const [tasks, setTaks] = useState(initialTasks);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  //   add new task
+  function handleAddTask(newTask) {
+    setTaks((tasks) => [...tasks, newTask]);
+  }
+
+  console.log(tasks);
+
   return (
     <div className="flex flex-col items-center justify-center bg-gray-900 rounded-md w-full   px-8 py-8 mt-4 shadow-xl">
-      <Form />
+      <Form
+        title={title}
+        onSetTitle={setTitle}
+        onSetDescription={setDescription}
+        description={description}
+        onAddTask={handleAddTask}
+      />
       <NavigateButtons />
       <TodoList />
     </div>
